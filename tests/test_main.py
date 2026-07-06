5cd87412ff2a9b1360cedbfa0fd056cd83d29588
sdprocode2023 <sdprocode2023@gmail.com>
2026-07-06 12:53:08 +0530

    Added pytest API tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,33 @@ from main import app
 
 client = TestClient(app)
 
+def get_token():
+    response = client.post(
+        "/login",
+        data = {
+            "username": "admin",
+            "password" : "python123"
+        })
+    assert response.status_code == 200
+    return response.json()["access_token"]
+
+
+def test_create_entry():
+    token = get_token()
+    response = client.post(
+        "/entry",
+        headers = {
+            "Authorization" : f"Bearer {token}"
+        },
+        json = {
+            "temperature" : 30.5,
+            "ethereum_price": 3500,
+            "joke": "pytest created this"
+        }
+    )
+    print(response.json())
+    assert response.status_code == 201
+
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
