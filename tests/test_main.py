c740953873cf48e00115d2fa78e95a3eeda3127f
sdprocode2023 <sdprocode2023@gmail.com>
2026-07-07 11:22:24 +0530

    Added 27 test

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,6 +13,65 @@ def get_token():
     assert response.status_code == 200
     return response.json()["access_token"]
 
+def test_search_not_found():
+    response = client.get("/search?keyword=xyzxyzxyz")
+    assert response.status_code == 200
+    assert response.json() == []
+
+def test_min_temperature():
+    token = get_token()
+    response = client.get(
+        "/entries?min_temp=20",
+        headers = {"Authorization": f"Bearer {token}"}
+    )
+    assert response.status_code == 200
+    data = response.json()
+    for entry in data:
+        assert entry["temperature"] >= 20
+
+def test_sort_temperature():
+    token = get_token()
+    response = client.get(
+        "/entries?sort=temperature",
+        headers = {"Authorization": f"Bearer {token}"}
+    )
+    assert response.status_code == 200
+
+def test_sort_temperature_descending():
+    token = get_token()
+    response = client.get(
+        "/entries?sort=temperature",
+        headers = {"Authorization": f"Bearer {token}"}
+    )
+    assert response.status_code ==200
+    data = response.json()
+    temperatures= [entry["temperature"] for entry in data]
+    assert temperatures == sorted(temperatures,reverse=True)
+
+def test_pagination():
+    token = get_token()
+    response = client.get(
+        "/entries?limit=2&offset=0",
+        headers = {"Authorization": f"Bearer {token}"}, 
+    )
+    assert response.status_code == 200
+    assert len(response.json()) <= 2
+
+def test_update_invalid_entry():
+    token = get_token()
+    response = client.put(
+        "/entry/999999",
+        headers = {"Authorization" : f"Bearer {token}"},
+        json = {
+            "temperature": 30,
+            "ethereum_price" : 3000,
+            "joke" : "test_update_invalid_entry"
+        }  
+    )
+    assert response.status_code == 404
+
+
+
 def test_update_entry():
     token = get_token()
     response = client.post(
@@ -40,6 +99,17 @@ def test_update_entry():
         )
 
     assert response.status_code == 200
+
+def test_update_entry_without_token():
+    response = client.put(
+        "/entry/{entry_id}",
+        json = {
+            "temperature" : 40,
+            "ethereum_price": 5000,
+            "joke" : "test_update_entry_without_token"
+        }
+    )
+    assert response.status_code == 401
 
 def test_delete_invalid_entry():
     token = get_token()
@@ -77,6 +147,9 @@ def test_delete_entry():
     )
     assert response.status_code == 200
 
+def test_delete_entry_without_token():
+    response = client.delete("/entry/{entry_id}")
+    assert response.status_code == 401
 
 def test_invalid_entry():
     token = get_token()
@@ -111,6 +184,10 @@ def test_stats():
     data = response.json()
     assert "total_entries" in data
 
+def test_stats_without_token():
+    response = client.get("/stats")
+    assert response.status_code == 401
+
 def test_search():
     token = get_token()
     response = client.get(
@@ -137,6 +214,10 @@ def test_latest_entry():
     assert "ethereum_price" in data
     assert "joke" in data
 
+def test_latest_entry_without_token():
+    response = client.get("/latest")
+    assert response.status_code == 401
+
 def test_get_entries():
     token = get_token()
     response = client.get(
@@ -148,6 +229,13 @@ def test_get_entries():
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data,list)
+
+def test_get_entries_withouttoken():
+    response = client.get("/entries",)
+    assert response.status_code == 401
+    
+    
+
 
 def test_create_entry():
     token = get_token()
@@ -164,6 +252,10 @@ def test_create_entry():
     )
     #print(response.json())
     assert response.status_code == 201
+
+def test_create_entry_without_token():
+    response = client.post("/entry")
+    assert response.status_code == 401
 
 def test_home():
     response = client.get("/")
@@ -199,6 +291,10 @@ def test_profile():
     )
     assert response.status_code == 200
 
+def test_profile_without_token():
+    response = client.get("/profile")
+    assert response.status_code == 401
+
 def test_invalid_login():
   response = client.post(
       "/login",
@@ -208,7 +304,3 @@ def test_invalid_login():
       }
   )
   assert response.status_code == 401
-
-def test_profile_without_token():
-    response = client.get("/profile")
-    assert response.status_code == 401
